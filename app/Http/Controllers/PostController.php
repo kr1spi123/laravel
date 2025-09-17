ef5b5af9bccc9329c9b1086174dd109f6caaaa10
Vlad <xy.lan65@gmail.com>
2025-09-17 18:39:12 +0300

    изучил create

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,9 +9,36 @@ use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    public function index(){
-        $posts = Post::where('is_published',1)->first();
+    public function index()
+    {
+        $posts = Post::where('is_published', 1)->first();
         dump($posts->title);
         dd('end');
+    }
+
+    public function create()
+    {
+        $postsArr = [
+            [
+                'title' => 'title of post from vscode',
+                'content' => 'balblsalbasl',
+                'image' => 'asdasl.jpg',
+                'likes' => 12,
+                'is_published' => 1,
+            ],
+            [
+                'title' => ' another title of post from vscode',
+                'content' => 'another balblsalbasl',
+                'image' => 'another asdasl.jpg',
+                'likes' => 22,
+                'is_published' => 1,
+            ],
+        ];
+
+        foreach ($postsArr as $item) {
+            Post::create($item);
+        }
+
+        dd('created');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -3,21 +3,12 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MyPlaceController;
 use App\Http\Controllers\PostController;
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 
-Route::get( '/my_page', [PostController::class, 'index'] );
-
+Route::get( '/posts', [PostController::class, 'index'] );
+Route::get('/posts/create', [PostController::class, 'create']);
