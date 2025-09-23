3972a753a5fe5925911f09955e74376d358823d7
Vlad <xy.lan65@gmail.com>
2025-09-23 15:02:37 +0300

    23. модифицируем интерфейс часть 4

--- resources/views/post/create.blade.php
+++ resources/views/post/create.blade.php
@@ -5,21 +5,32 @@
         @csrf
         <div class="mb-3">
             <label for="title" class="form-label">Title</label>
-            <input type="text" class="form-control" id="title" placeholder="title" name="title">
+            <input type="text" class="form-control" id="title" placeholder="title" name="title" value="{{ old('title') }}">
+            @error('title')
+            <p class="text-danger">{{$message}}</p>
+            @enderror
         </div>
         <div class="mb-3">
             <label for="content" class="form-label">Content</label>
-            <textarea class="form-control" id="content" placeholder="Content" name="content"></textarea>
+            <textarea class="form-control" id="content" placeholder="Content" name="content">{{ old('content') }}</textarea>
+            @error('content')
+            <p class="text-danger">{{$message}}</p>
+            @enderror
         </div>
         <div class="mb-3">
             <label for="image" class="form-label">Image</label>
-            <input type="text" class="form-control" id="image" placeholder="image" name="image">
+            <input type="text" class="form-control" id="image" placeholder="image" name="image" value="{{ old('image') }}">
+            @error('image')
+            <p class="text-danger">{{$message}}</p>
+            @enderror
         </div>
         <div class="mb-3">
             <label for="category">Category</label>
             <select class="form-control" id="category" name="category_id">
                 @foreach ($categories as $category)
-                <option value="{{ $category->id }}">{{ $category->title }}</option>
+                <option
+                    {{ old('category_id') == $category->id ? 'selected' : '' }}
+                value="{{ $category->id }}">{{ $category->title }}</option>
                 @endforeach
             </select>
         </div>
