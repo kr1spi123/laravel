f53e6d3e6e887c3884ea106a97355632b52c272b
Vlad <xy.lan65@gmail.com>
2025-09-25 19:10:23 +0300

    26. Класс Request

--- app/Http/Controllers/Post/StoreController.php
+++ app/Http/Controllers/Post/StoreController.php
@@ -4,19 +4,13 @@ namespace App\Http\Controllers\Post;
 
 use App\Models\Post;
 use App\Http\Controllers\Controller;
-
+use App\Http\Requests\Post\StoreRequest;
 
 class StoreController extends Controller
 {
-    public function __invoke()
+    public function __invoke(StoreRequest $request)
     {
-        $data = request()->validate([
-            'title' => 'string',
-            'content' => 'string',
-            'image' => 'string',
-            'category_id' => '',
-            'tags' => '',
-        ]);
+        $data = $request->validated();
         $tags = $data['tags'];
         unset($data['tags']);
 
--- app/Http/Controllers/Post/UpdateController.php
+++ app/Http/Controllers/Post/UpdateController.php
@@ -4,18 +4,12 @@ namespace App\Http\Controllers\Post;
 
 use App\Models\Post;
 use App\Http\Controllers\Controller;
-
+use App\Http\Requests\Post\UpdateRequest;
 
 class UpdateController extends Controller
 {
-    public function __invoke(Post $post){
-         $data = request()->validate([
-            'title' => 'string',
-            'content' => 'string',
-            'image' => 'string',
-            'category_id' => '',
-            'tags' => '',
-        ]);
+    public function __invoke(UpdateRequest $request, Post $post){
+         $data = $request->validated();
         $tags = $data['tags'];
         unset($data['tags']);
 
